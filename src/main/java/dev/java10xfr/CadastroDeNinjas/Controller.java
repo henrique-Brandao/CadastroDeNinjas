0faeca75a1c12b5dde7838500516d64f12c622c2
HENRIQUE GABRIEL DOS SANTOS BRANDÃO <232.953563@universidadesenaicimatec.edu.br>
2025-09-11 20:00:17 -0300

    Criado a Controller

---
+++ src/main/java/dev/java10xfr/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10xfr.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/testea")
+    public String test() {
+        return "Test";
+    }
+}
