544c34da2f5a1932754a8dc4138f75c076a7caab
henrique-brandao <henriquefr321@gmail.com>
2025-09-26 10:02:10 -0300

    feat: Adiciona Lombok para reduzir boilerplate

--- src/main/java/dev/java10xfr/CadastroDeNinjas/CadastroDeMissoes/MissoesModel.java
+++ src/main/java/dev/java10xfr/CadastroDeNinjas/CadastroDeMissoes/MissoesModel.java
@@ -3,11 +3,17 @@ package dev.java10xfr.CadastroDeNinjas.CadastroDeMissoes;
 
 import dev.java10xfr.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/dev/java10xfr/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10xfr/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,12 +2,18 @@ package dev.java10xfr.CadastroDeNinjas.Ninjas;
 
 import dev.java10xfr.CadastroDeNinjas.CadastroDeMissoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // Entity transforma uma classe em uma entidade no dataBase
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,37 +29,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "Missoes_id") // Foreing key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
